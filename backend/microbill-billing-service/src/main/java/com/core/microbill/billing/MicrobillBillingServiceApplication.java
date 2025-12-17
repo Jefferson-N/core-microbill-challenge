55ac2013babebac0d0fd26793331c3abfb8beb99
Jf <jeffer_12123@hotmail.com>
2025-12-17 09:52:44 -0500

    develop: Backend proyect structure

---
+++ backend/microbill-billing-service/src/main/java/com/core/microbill/billing/MicrobillBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.core.microbill.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobillBillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobillBillingServiceApplication.class, args);
+	}
+
+}
