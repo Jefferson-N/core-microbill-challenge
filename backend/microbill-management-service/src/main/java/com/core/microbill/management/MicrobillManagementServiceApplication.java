1a1d16b052e9d268f44d733aa4b17219ab889b12
Jf <jeffer_12123@hotmail.com>
2025-12-17 11:42:17 -0500

    develop: Add management project

---
+++ backend/microbill-management-service/src/main/java/com/core/microbill/management/MicrobillManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.core.microbill.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobillManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobillManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/microbill-management-service/src/test/java/com/core/microbill/management/MicrobillManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.microbill.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobillManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
